3da72d08bc9a9f594bbe4d9952187d966545eb17
Jie Zhang <zhangjiew@gmail.com>
2021-07-19 14:36:01 -0600

    two Routes added

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,20 @@
-import React from 'react';
-
+import React from "react";
+import { useHistory } from "react-router-dom";
 export default function SavedList(props) {
+  const history = useHistory();
+  const shiftBack = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
+      {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <div onClick={shiftBack} className="home-button">
+        Home
+      </div>
     </div>
   );
 }
